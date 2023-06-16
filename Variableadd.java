be4ac2e4de24cf0234cea7f1ec5c0d4cce25e0cd
EksanjD <136789560+EksanjD@users.noreply.github.com>
2023-06-17 00:08:07 +0530

    Variableadd.java

---
+++ Variableadd.java
@@ -0,0 +1,10 @@
+public class Variadd{
+    public static void main(String args[]){
+    float a=10.11f;
+    float b=12.12f;
+    float c=11.31f;
+    int d=(int)a+(int)b+(int)c;
+    System.out.println(d);
+    }
+
+}

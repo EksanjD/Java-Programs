30008bc137be951b47372a855eb6d87a77b0dc9d
EksanjD <136789560+EksanjD@users.noreply.github.com>
2023-06-17 12:40:23 +0530

    Add files via upload

---
+++ Split.java
@@ -0,0 +1,9 @@
+public class Split{
+    public static void main(String args[]){
+        float a=96.64f;
+        int b=(int)a;
+        float c=a-b;
+        System.out.println(b);
+        System.out.print(c);
+         }
+}

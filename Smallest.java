570e2b7c1329ea06d774cc7b833e44f3f9d29810
EksanjD <136789560+EksanjD@users.noreply.github.com>
2023-06-17 10:19:13 +0530

    Add files via upload

---
+++ Smallest.java
@@ -0,0 +1,14 @@
+public class Smallest{
+    public static void main(String args[]){
+        int a=25;
+        int b=37;
+        int c=29;
+
+        if(b<a){System.out.println("b is less than than a");}
+        else if(c<a){System.out.println("c is less than a");}
+        else {System.out.println("a is smallest");}
+    }
+     
+
+
+}

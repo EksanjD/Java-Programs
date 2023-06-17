bb83b887ef81fc540b87cee327b35e6ab46ec7d2
EksanjD <136789560+EksanjD@users.noreply.github.com>
2023-06-17 12:17:03 +0530

    Add files via upload

---
+++ Gettotal.java
@@ -0,0 +1,12 @@
+public class Gettotal{
+        static void getTotal(int a, int b){
+        int c=a+b;
+        System.out.println(c);
+    }
+        public static void main(String args[]){
+        getTotal(12,13);
+        }
+        
+
+
+}
